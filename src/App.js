d794bf1c872005b7165a5d662a12e8ce491f08dd
Ankush Yadav <yadavankush1811@gmail.com>
2025-09-03 19:45:54 +0530

    Sale bar is added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Nav  from './components/Nav';
 import { Navbar } from './components/Navbar';
+import {Salebar} from './components/Salebar';
+
 function App() {
   return (
     <>
     <Nav/>
     <Navbar/> 
+    <Salebar/>
     </>
   );
 }
---
+++ src/components/Salebar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export function Salebar(){
+    return (
+        <>
+            <ul class="nav justify-content-center">
+                <li class="nav-item">
+                    <a className="nav-link active text-black fw-semibold" aria-current="page" href="#">New Styles On Sale: Up To 40% Off</a>
+                    <a className="text-black active text-black mx-4 fw-semibold text-[10cm]" aria-current="page" href="#">Shop All Our New Markdowns</a>
+                    
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#"></a>
+                </li>
+            </ul>
+        </>
+    );
+}
